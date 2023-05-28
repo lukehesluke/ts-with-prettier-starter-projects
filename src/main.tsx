c82b550fdaeb592f8a6a72b7db78888b99e395d2
Luke Winship <luke.winship@gmail.com>
2023-05-28 13:46:03 +0100

    fix: Have `npm test` run against .tsx files

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
-import React, { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = createRoot(document.getElementById('root') ?? (() => { throw new Error('Root element not found'); })());
+const root = createRoot(
+  document.getElementById('root') ??
+    (() => {
+      throw new Error('Root element not found');
+    })()
+);
 root.render(
   <StrictMode>
     <App />
